cd1cc61732bcf75b0ef3ebce88eced5067918c74
Dee Williams <deejmwilliams@gmail.com>
2023-08-10 12:41:31 -0400

    Drag and drop implemented for item movement

--- src/components/LocationList.tsx
+++ src/components/LocationList.tsx
@@ -5,13 +5,17 @@ import ListGroup from 'react-bootstrap/ListGroup';
 import Badge from 'react-bootstrap/Badge';
 //Redux
 import {useSelector, useDispatch} from 'react-redux';
-import { selectLocations } from '../slices/allLocationSlice.tsx';
+import { selectLocations, editLocation } from '../slices/allLocationSlice.tsx';
 import {selectLocation, setLocation} from '../slices/locationSlice.tsx';
+import {setLocationItems, selectLocationItems} from '../slices/locationItemSlice.tsx';
+import { selectDraggedItem, resetDraggedItem } from '../slices/dragSlice.tsx';
 //Types
-import {Location} from '../types.tsx';
+import {Location, Item} from '../types.tsx';
 //Components
 import LocationDeletePopup from './LocationDeletePopup.tsx';
 import LocationEditPopup from './LocationEditPopup.tsx';
+//Methods
+import { moveItem, getLocation } from '../methods.tsx';
 
 type locationProps = {
     type:string;
@@ -20,29 +24,73 @@ type locationProps = {
 const LocationList = ({type}:locationProps):JSX.Element => {
     const dispatch = useDispatch();
     const locations:Location[] = useSelector(selectLocations);
-    const activeLocation = useSelector(selectLocation);
+    const activeLocation:Location = useSelector(selectLocation);
+    const draggedItem:Item = useSelector(selectDraggedItem);
+    const locationItems:Item[] = useSelector(selectLocationItems);
     const [deleting, setDeleting] = useState(false);
     const [editing, setEditing] = useState(false);
+    const [destination, setDestination] = useState(activeLocation);
 
     type ListItemProps = {
         location:Location;
     }
 
-    const checkDelete = (location:Location) => {
+    const checkDelete = (location:Location):void => {
         dispatch(setLocation(location));
         setDeleting(true);
     }
 
-    const startEdit = (location:Location) => {
+    const startEdit = (location:Location):void => {
         dispatch(setLocation(location));
         setEditing(true);
+    }
+
+    const handleDragOver = (event:React.DragEvent, loc:Location):void => {
+        event.preventDefault();
+        if (destination.lid !== loc.lid) {
+            setDestination(loc);
+        }
+    }
+
+    const handleDragLeave = (event:React.DragEvent, loc:Location):void => {
+        event.preventDefault();
+        if (destination.lid === loc.lid) {
+            setDestination({'name': '', 'lid': '', 'gid': '', type: 'location', timestamp: '', item_ids: []});
+        }
+    }
+
+    const handleDrop = (loc:Location):void => {
+        if (draggedItem.lid !== loc.lid) {
+            moveItem(loc.lid, draggedItem.gid, draggedItem.lid, draggedItem.iid)
+            .then(() => {
+                //Add new item ID to new location
+                dispatch(editLocation({...loc, item_ids: [...loc.item_ids, draggedItem.iid]}))
+                getLocation(draggedItem.gid, draggedItem.lid)
+                .then(response => {
+                    //Remove item ID from old location
+                    const newItems:string[] = response.data.item_ids.filter((item_id:string) => item_id !== draggedItem.iid);
+                    dispatch(editLocation({...response.data, item_ids: [...newItems]}));
+                    //Rerender location items
+                    dispatch(setLocationItems(locationItems.filter(item => newItems.includes(item.iid))));
+                })
+            })
+            .catch(e => console.log(e))
+        }
+        setDestination({'name': '', 'lid': '', 'gid': '', type: 'location', timestamp: '', item_ids: []});
+        dispatch(resetDraggedItem());
     }
     
     const LocationListItem = ({location}: ListItemProps):JSX.Element => {
         let selected = location.lid === activeLocation.lid;
-        return <ListGroup.Item 
+        return <div className="droppable" 
+                onDragOver={(e) => handleDragOver(e, location)}
+                onDragLeave = {(e) => handleDragLeave(e, location)}
+                onDrop={() => handleDrop(location)}
+                key={location.lid}>
+                <ListGroup.Item 
                 onClick={()=> dispatch(setLocation(location))} 
-                className={`location__entry ${selected ? 'active': ''}`}>
+                className={`location__entry ${selected ? 'active': ''}`}
+                variant={location.lid === destination.lid ? 'primary': ''}>
                     <span className="location__text">
                     {location.name}
                     </span>
@@ -53,6 +101,7 @@ const LocationList = ({type}:locationProps):JSX.Element => {
                 </div> :
                 <React.Fragment></React.Fragment>}
                 </ListGroup.Item>
+                </div>
     }
 
     const locationsOfType = locations.filter((location:Location) => location.type === type);
